57f1a6e0d06cda01d6c2637af603cb41c9bc4aa5
Marco Schanz <mschanz@uni-koblenz.de>
2016-06-29 21:49:52 +0200

    update README

--- modules/documentLearning/README.rst
+++ modules/documentLearning/README.rst
@@ -3,15 +3,19 @@
    :target: https://github.com/fuuman/101worker/tree/master/modules/documentLearning
    :alt: documentLearning Logo
 
-
-zu Usage:
-Die Jsondateien müssen in den Projektordner eingefügt werden.
-Der z Wert innerhalb der Datein jsonExtractor.py muss angepasst werden.
+Usage
+-----
+Die Jsondateien müssen in den Projektordner eingefügt werden sowie die wiki-content.json
+Der z Wert innerhalb der Datein jsonExtractor.py muss angepasst werden, sodass er auch wirklich nur json Dateien innerhalb des Ordners einliest.
 Der eigentlich Aufruf befindet sich in NatLan.py ganz unten.
 
-zu Readme:
+External Code
+-------------
+We took the Naive Bayes algorithm from the NLTK.
+For more information, visit http://www.nltk.org/ , or more specifically http://www.nltk.org/api/nltk.classify.html?highlight=naive%20bayes#module-nltk.classify.naivebayes
 
-Unsere Aufgabe:
+Unsere Aufgabe
+--------------
 Document learning: The different namespaces on the wiki come with different expected sections. For instance, each page in the namespace
 "Contribution" is supposed to contain a section "Characteristics".
 The complete set of possible (actual) section names can be found by extracting all section names from the dump wiki-content.json.
@@ -19,10 +23,26 @@ Apply machine learning to provide a prediction model with the namespace as the i
 By applying the prediction model to further pages, try to identify pages with missing or unexpected sections.
 A missing section means that prediction suggests that a section should be there for the page's namespace at hand; likewise for an unexpected section.
 
-Was wir geschafft haben:
+Interpretation unseres Ergebnisses 
+----------------------------------
 Wir haben aus den Json-Dateien die Namespaces und ihre jeweiligen Sections extrahiert. Danach haben wir anhand der Daten ein Prediction Model mit machine learning erstellt.
-Uns fehlt dabei jedoch der Übergang, zu testen und damit missing und unexspected in beliebige json Dateien einzutragen.
-Wir wollten mit dem Prediction Model die jeweiligen sections der namespaces vergleichen und wenn ein neues erkannt wird, ist es unexspected und wenn eines fehlt, dann ist es missing.
-Probleme traten jedoch dabei auf, die Prediction Models jeweligen Namespaces zu zuweisen, sodass wir nicht mehr fortfahren konnten mit unserer Arbeit.
-Diese Probleme traten vor allem auf, durch mangelnde Python-Kenntnisse und der gleichzeitig zu erlernenden NLTK Libary. Mit mehr Zeit wäre uns wahrscheinlich ein abschließendes Ergebnis gelungen.
+Uns fehlt dabei jedoch der Übergang, zu testen und damit missing und unexspected in jeweilige json Dateien einzutragen.
+Wir wollten mit dem Prediction Model die jeweiligen sections der namespaces vergleichen und wenn ein neues erkannt wird, ist es unexpected und wenn eines fehlt, dann ist es missing.
+Bei den ausgegebenen Dictionaries wurde jeder Section eines Namespaces eine bestimmte approximierte Wahrscheinlichkeit über den Bayes Algorithmus zugewiesen.
+In 'List for comparison for tests' stehen alle Sections dieses Namespaces drin, die nicht über unser Prediction Model aussortiert wurden, wenn also die Wahr-
+scheinlichkeit der Section über 10% lag.
+
+Probleme
+--------
+Probleme traten jedoch dabei auf, die Prediction Models jeweiligen Namespaces zuzuweisen, sodass wir nicht mehr fortfahren konnten mit unserer Arbeit.
+Diese Probleme traten vor allem auf, durch mangelnde Python-Kenntnisse und der gleichzeitig zu erlernenden NLTK Library. Mit mehr Zeit wäre uns wahrscheinlich ein abschließendes Ergebnis gelungen.
 Wir wissen, dass dies unglaubwürdig klingt, jedoch haben wir zeitgemäß vor zwei Wochen mit der Implementation begonnen.
+
+Developers
+----------
+- Marco Schanz
+- Diana Richter
+- Marius Beckmann
+- Frank Schaust
+- André-Gilbert Thomas
+- Isabelle Kuhlmann
--- modules/documentLearning/__init__.py
+++ modules/documentLearning/__init__.py
@@ -2,10 +2,8 @@ from .jsonExtractor import *
 from .jsonSoftPro import *
 import json
 import nltk
-from nltk.probability import *
 import random
-from nltk.corpus import names
-from nltk.probability import DictionaryProbDist
+from os import getcwd
 
 config = {
     'wantdiff': False,
@@ -18,75 +16,140 @@ config = {
 }
 
 def run(context):
-
-    all = []
-    all2 = []
-    all3 = []
-    all4 = []
-    all5 = []
-    nochSoEine = []
-    predModFinal = []
-
+    # will get all entries of our Featureset in the function 'rawFeatureset
+    allRawFeaturesets = []
+    # this contains later on all tuples extracted of our rawFeatureset
+    rfTupleList = []
+    # this contains later on our list which can be interpreted by our machine learning algorithm
+    finalFeatureset = []
+    # gets us the list of the json data files in the projectfile. Each list entry is a string.
     json_data = jsonExtractor.getJsonData()
-    for i in json_data:
-        with open(getcwd() + "/modules/documentLearning/wiki-content.json") as json_file:
-            data = json.load(json_file)
-    namespaces = jsonSoftPro.getNamespaces(data)
 
-    # This function is a getter for Namespaces and Features we are looking for within a json data
-    # and returns us a "featureset" consisting of the namespace and the sections for it as list.
-    def giv(json):
+    #########################################################################################################################
+    # This function returns us a raw "featureset" consisting of the namespace and the sections for it as list.
+    # e.g. [[(['Concept'], ['Headline', 'Metadata', 'Description'])], [(['Technology'], ['Headline', 'Metadata'])],..]]
+    # input is a single json data
+    def rawFeatureset(json):
+        # this for-loop extracts in each json data we give in the namespace and sectionname
+        # and transforms it in a list we can use in further steps
         for sec in json:
-            if len(json_data) == len(all):
+            # if our allRawFeaturesets list is as long as the json_data we stop.
+            # Due to problems the program read more data as it should, this seems to fix it
+            if len(json_data) == len(allRawFeaturesets):
                 break
             labeled_names = ([(elem, sec) for elem in json])
-            featuresets = [(jsonSoftPro.getNamespaces(json), find_features(json)) for (n, s) in labeled_names]
-            all.append(featuresets)
-        return all
+            featuresets = [(jsonSoftPro.getNamespaces(json), jsonSoftPro.getSectionnames(json)) for (n, s) in
+                           labeled_names]
+            allRawFeaturesets.append(featuresets)
 
+        return allRawFeaturesets
 
-    # This function is return the sectionnames within the json data
-    def find_features(json):
-        t = jsonSoftPro.getSectionnames(json)
-        return t
+    ########################################################################################################################
+    #######################################################################################################################
+    # "transform" transforms us our raw Featureset into a list our machine learning algorithm can deal with
+    # e.g. [({'Namespace': 'Concept'}, 'Headline'), ({'Namespace': 'Concept'}, 'Description'),...
+    def transform(allRawFeaturesets):
+        # first we flatten our raw Featureset, till we got a simple nested list
+        # [['Concept'], ['Metadata', 'Description', 'Headline'], ['Technology'], ['Metadata', 'Headline'],...]]
+        flattened = [val for sublist in lf for val in sublist]
+        flattened2 = [val for sublist in flattened for val in sublist]
 
+        # this nested for-loop applies the namespace we have with each section of it as tuples
+        # e.g. [('Description', 'Concept'), ('Headline', 'Concept'), ('Metadata', 'Concept'),..]
+        l = 0
+        k = 1
+        z = 0
+        for entry in flattened2:
+            for entry in flattened2:
+                # this try-catch block guarantees that we do not run out of bound within the list.
+                # if we exceed the list length with our parameter z the except block will return our z to 0
+                # and we can go on
+                try:
+                    t = (flattened2[k][z], flattened2[l][0])
+                    t = tuple(t)
+                    rfTupleList.append(t)
 
-    # sums up the namespaces with their sections and returns them as list
-    def test(input):
+                    z = z + 1
+                    f = z
+
+                except:
+                    z = z - f
+                    k = k + 2
+                    l = l + 2
+
+        # this for-loop will transform every first tuple entry as a dictionary
+        for (n, s) in rfTupleList:
+            s = ({'Namespace': s}, n)
+            finalFeatureset.append(s)
+        return finalFeatureset
+
+    #######################################################################################################################
+    ########################################################################################################################
+    # this function sorts our tuples by their namespaces
+    # so that all namespaces are listed after another
+    # e.g. first all tuples of 'Concept' and then all tuples of 'Technology'
+    # this is a approach to compare our outcoming prediction model with lists of concepts and sections
+    # in other json data. It is not useful by now.
+    def sortByNamespaces(finalFeatureset):
+        # we extract all namespaces out of the wiki-content.json
+        # and save them as a list in namespace. We are just passing our finalFeatureset through, which is shuffled anyway
+        # later on.
+        for i in json_data:
+            with open(getcwd() + "/modules/documentLearning/wiki-content.json") as json_file:
+                data = json.load(json_file)
+        namespaces = jsonSoftPro.getNamespaces(data)
+
         neueListe2 = []
         finalList = []
         for pg in namespaces:
             neueListe = []
-            for entry in input:
+            for entry in finalFeatureset:
                 le = entry[0]
                 if (le['Namespace'] == pg):
                     neueListe.append(entry)
             neueListe2.append(neueListe)
         list2 = [x for x in neueListe2 if x != []]
-        tmp = predModExtractor(list2)
-
-        print(tmp)
+        tmp = predMod(list2)
         finalList.append(tmp)
 
+    #######################################################################################################################
+    #######################################################################################################################
 
     # Creates Prediction Model with NaiveBayes-Algorithm
-    def predModExtractor(neueListe):
+    def predMod(finalFeatureset):
         predMod = []
-        for k in neueListe:
+        for k in finalFeatureset:
+            # this step will mix up our finalFeatureset on test purposes
             random.shuffle(k)
+            # the first 5 entries in our finalFeatureset will be used as training set for
+            # our algorithm
             train_set = k[:5]
-            test_set = k[5:]
+            # everything over the 5th entry will be tested on (not used)
+            # test_set = k[5:]
+            # our classifier is the Naive Bayes Classifier
             classifier = nltk.NaiveBayesClassifier.train(train_set)
-            nltk.classify.accuracy(classifier, test_set)
+
+            # nltk.classify.accuracy(classifier, test_set)
+            # this line is just for "creating" a prob-object we can use, the input is not relevant
             d2 = classifier.prob_classify({'Namespace': 'Concept'})
-            t = d2.samples()
+            # the sampleList are all sectionnames in our trainigsset for a given Namespace
+            sampleList = d2.samples()
 
             # We decided that sections under 10 percent are sorted out
-            for l in t:
+            # e.g. is Illustration under 10 percent in our prediction it will not be in the list
+            # which might look like ['Headline','Metadata',...]
+            # this for-loop will also print out the probability for each section
+            for l in sampleList:
                 if d2.prob(l) > 0.1:
                     print({l: d2.prob(l)})
                     predMod.append(l)
+
+                    # is printing our comparison list for the different namespaces we wanted to use for testing
+            print({'List for comparison for tests': list(sampleList)})
         return predMod
+
+    ########################################################################################################################
+    ########################################################################################################################
 
 
     # in progress
@@ -98,44 +161,16 @@ def run(context):
 
         return
 
+    ########################################################################################################################
+    #######################################################################################################################
 
-    # Creates a list of tuples. Each tuple consists of the namespace and one of its sections
-    def transform(flattened2):
-        l = 0
-        k = 1
-        z = 0
-        i = 0
-
-        for bla in flattened2:
-
-            for bla in flattened2:
-
-                try:
-                    t = (flattened2[k][z], flattened2[l][0])
-                    t = tuple(t)
-                    all3.append(t)
-
-                    z = z + 1
-                    f = z
-
-                except:
-                    z = z - f
-                    k = k + 2
-                    l = l + 2
-        return all3
-
-
-
+    # main
+    # reads in our list of json datas one json data after another passing it to rawFeatureset
     for i in json_data:
-        giv(i)
-    lf = giv(i)
-    lf2 = giv(i)
-    flattened = [val for sublist in lf for val in sublist]
-    flattened2 = [val for sublist in flattened for val in sublist]
-    all4 = transform(flattened2)
-    for (n, s) in all4:
-        s = ({'Namespace': s}, n)
-        all5.append(s)
-    test(all5)
+        rawFeatureset(i)
+    # lf catches the list of all results of 'rawFeaturesets'
+    lf = rawFeatureset(i)
+    fF = transform(lf)
+    sortByNamespaces(fF)
 
-    # tester(json_data)
+
